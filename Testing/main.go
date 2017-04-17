a32ff7b6af52ec382929d262f48aac745d5eb76c
Daniel Trinh <dantrinh@seas.upenn.edu>
2017-04-17 19:11:37 -0400

    Updating Tests

---
+++ Testing/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dantrinh/cis193-finalproject"
+)
+
+func main() {
+
+	a, _ = stats.Mean([]float64{1, 2, 3, 4, 5})
+	fmt.Println(a) // 3
+
+}
--- test.go
+++
@@ -1,8 +0,0 @@
-package stats
-
-func test() {
-	var data = []float64{1, 2, 3, 4, 4, 5}
-
-	mean, _ := Mean(data)
-	fmt.Println(mean) // 3.5
-}
