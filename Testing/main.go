a9277d139b33cba74d18d270545ddef70fcb686e
Daniel Trinh <dantrinh@seas.upenn.edu>
2017-04-17 21:46:17 -0400

    Updating Tests

--- Testing/main.go
+++ Testing/main.go
@@ -11,4 +11,37 @@ func main() {
 	a, _ := cis193stats.Mean([]float64{1, 2, 3, 4, 5})
 	fmt.Println(a) // 3
 
+	a, _ = cis193stats.Median([]float64{1, 2, 3, 4, 5, 6, 7})
+	fmt.Println(a) // 4
+
+	a, _ := cis193stats.Min(d)
+	fmt.Println(a) // 1.1
+
+	a, _ = cis193stats.Max(d)
+	fmt.Println(a) // 5
+
+	a, _ = cis193stats.SumAll([]float64{1.1, 2.2, 3.3})
+	fmt.Println(a) // 6.6
+
+	m, _ := cis193stats.Mode([]float64{5, 5, 3, 3, 4, 2, 1})
+	fmt.Println(m) // [5 3]
+
+	/*a, _ = stats.PopulationVariance([]float64{1, 2, 3, 4, 5})
+	fmt.Println(a) // 2
+
+	a, _ = stats.SampleVariance([]float64{1, 2, 3, 4, 5})
+	fmt.Println(a) // 2.5
+
+	a, _ = stats.MedianAbsoluteDeviationPopulation([]float64{1, 2, 3})
+	fmt.Println(a) // 1
+
+	a, _ = stats.StandardDeviationPopulation([]float64{1, 2, 3})
+	fmt.Println(a) // 0.816496580927726
+
+	a, _ = stats.StandardDeviationSample([]float64{1, 2, 3})
+	fmt.Println(a) // 1
+
+	a, _ = stats.Percentile([]float64{1, 2, 3, 4, 5}, 75)
+	fmt.Println(a) // 4*/
+
 }
